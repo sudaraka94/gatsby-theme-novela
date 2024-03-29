78b054992112c8b1041ba6d857026426f6cd7dc6
Sudaraka Jayathilaka <sudarakayasindu@gmail.com>
2024-03-29 18:35:56 +0800

    initial commit

---
+++ src/pages/404.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import Layout from "../components/Layout";
+import Section from "../components/Section";
+import SEO from "../components/SEO";
+import Headings from "../components/Headings";
+
+function NotFoundPage() {
+  return (
+    <Layout>
+      <SEO />
+      <Section>
+        <div style={{ marginTop: "100px" }}>
+          <Headings.h1>404: Page Not Found</Headings.h1>
+        </div>
+      </Section>
+    </Layout>
+  );
+}
+
+export default NotFoundPage;
